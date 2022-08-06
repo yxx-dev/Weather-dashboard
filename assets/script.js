f7b65f8c48cbe1d58b8c3c03392ac05b1ef7c79b
yxx-dev <hermitlun@gmail.com>
2022-08-05 23:12:22 -0700

    search history

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,37 @@
 let city
-let cityHistory = []
+let searchHistory = []
+let newSearchHistory
+let searchHistoryListEl = $('.search-history')
 let lat = 0
 let lon = 0
 let geoCoding 
 let weather
 
+
 //read city search history from localStorage
 $(window).on('load',function(){
-    let searchHistory = localStorage.getItem('searchHistory');
-    if(searchHistory !== null) {
+    if(localStorage.getItem('searchHistory') !== null) {
+        searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
         for (i=0; i<searchHistory.length; i++) {
-            let newSearchHistory = document.createElement('button');
+            newSearchHistory = document.createElement('button');
+            newSearchHistory.textContent = searchHistory[i];
             newSearchHistory.setAttribute('type','button');
             newSearchHistory.setAttribute('class','btn-width btn btn-secondary');
+            $(searchHistoryListEl).append(newSearchHistory);
+            if (i>3) {
+                break;
+            };
         }
     }
     
+})
+
+//clear search history
+$('#clear-button').on('click',function(){
+    //not functioning yet
+    localStorage.clear();
+    localStorage.removeItem('searchHistory');
+    searchHistoryListEl.children().remove();
 })
 
 $('#searchButton').on('click', function(){
@@ -39,8 +55,32 @@ function getWeather() {
             } 
             console.log(data);
             console.log(data[0].lat, data[0].lon);
+            
+            console.log(searchHistory);
+            searchHistory.unshift(city);
+            if (searchHistory.length>6) {
+                searchHistory.pop();
+            }
+            console.log(searchHistory);
+            localStorage.setItem('searchHistory',JSON.stringify(searchHistory));
+            
+            newSearchHistory = document.createElement('button');
+            newSearchHistory.textContent = city;
+            newSearchHistory.setAttribute('type','button');
+            newSearchHistory.setAttribute('class','btn-width btn btn-secondary');
+            $(searchHistoryListEl).prepend(newSearchHistory);
+            //for (i=0)
+            console.log($(searchHistoryListEl).length);
+            if($(searchHistoryListEl).children().length>6) {
+                searchHistoryListEl.children().last().remove();
+            }
+            
+            
+
             lat = data[0].lat;
             lon = data[0].lon;
+
+
 
             //check if error & 404 x2
 
